3e977ad577390944e7e67448009341b6abcda2ab
sideroad <sideroad.jp@gmail.com>
2012-08-19 00:39:36 +0900

    testem task

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 grunt-tasks
 ===========
 
-grunt tasks
+Install
+Download grunt-tasks and add path of load task into grunt.js.
+```javascript
+grunt.loadTasks('path/to/grunt-tasks');
+```
+
+testem
+------
+
+
+### All browsers test
+```javascript
+grunt.initConfig({
+  testem : {
+    ci : []
+  }
+});
+```
+
+### Browser selected
+```javascript
+grunt.initConfig({
+  testem : {
+    ci : ['chrome','safari']
+  }
+});
+```
---
+++ testem.js
@@ -0,0 +1,41 @@
+/*
+ * grunt
+ * https://github.com/cowboy/grunt
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * http://benalman.com/about/license/
+ */
+
+module.exports = function(grunt) {
+
+  var exec = require('child_process').exec;
+  
+  grunt.registerMultiTask( 'testem', 'Execute testem.', function() {
+    
+    var done = this.async(),
+      browsers = (this.data.length) ? ' -b ' + this.data.join(',') : '';
+
+    exec( 'testem ci'+browsers, {}, function( code, stdout, stderr ){
+        var tests = +(stdout.match(/\n# tests\s+(\d+)/)||[0])[1],
+          pass = +(stdout.match(/\n# pass\s+(\d+)/)||[0])[1],
+          fail = +(stdout.match(/\n# fail\s+(\d+)/)||[0])[1],
+          not = (stdout.match(/\nnot ([^\n]+)/g)||['']).join('');
+        
+        if( tests != pass ||
+            fail ||
+            not ||
+            stderr ) {
+          grunt.log.error(not);
+          grunt.log.error(''+fail+'/'+tests+' assertions failed');
+          done(false);
+        } else {  
+          grunt.log.writeln(''+tests+' assertions passed');
+          done(true);
+        }
+    });
+    grunt.log.writeln('Now testing...');
+  });
+
+
+};
